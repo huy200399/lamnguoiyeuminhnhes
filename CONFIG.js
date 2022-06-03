7fd4e24afe68037f64755894661515f90c292c4b
Your Name <you@example.com>
2022-06-03 15:31:50 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,8 @@
+const CONFIGDATA = {
+    titleHeader: "Làm người yêu mình nhé I Love You",
+    descriptionHeader: "Nếu bạn thoát ra và không trả lời thì có nghĩa là bạn đã ĐỒNG Ý rồi đấy ^^",
+    buttonYes: "Đồng Ý",
+    buttonNo: "Không! Còn lâu nhé.",
+    titleModar: "Cảm ơn bạn yêu ",
+    descriptionModar: 'Mình  biết là bạn sẽ đồng ý làm người yêu mình mà ^^ </br> Cám ơn bạn đã đồng ý để cho mình được che chở và bảo vệ bạn.Đối với mình, bạn là người xinh đẹp nhất thế gian này :)) </br> "Thương bạn nhiều đến mấy, bạn sẽ hiểu thôi. </br> Cảm ơn bạn đã đến và đồng hành cùng với mình. </br> Mình yêu bạn Võ Thị Mỹ Lệ "',
+};
